f448e64f3a0d6e6e1b66aeee7941d38208e24402
1ankurchahar <1ankurchahar@gmail.com>
2022-12-31 19:57:34 +0530

    media query added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactEcharts from "echarts-for-react";
+
+import WINEDATA from "./Wine-Data.json";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const alcohol = WINEDATA.map((data) => data["Alcohol"]);
+    const alcoholLabels = Array.from(new Set(alcohol));
 
+    const malicAcid = WINEDATA.map((data) => data["Malic Acid"]);
+    const colorIntensity_hue = WINEDATA.map((data) => {
+        let d = [];
+        d.push(+data["Color intensity"]);
+        d.push(+data["Hue"]);
+        return d;
+    });
+
+    const optionBar = {
+        xAxis: {
+            type: "category",
+            name: "Alcohol",
+            data: alcoholLabels,
+            axisLabel: {
+                formatter: "{value} ",
+                align: "center",
+            },
+        },
+        yAxis: {
+            type: "value",
+            name: "Malic Acid",
+        },
+        series: [
+            {
+                data: malicAcid,
+                type: "bar",
+            },
+        ],
+    };
+
+    const optionScatter = {
+        xAxis: {
+            type: "category",
+            name: "Color Intensity",
+            axisLabel: {
+                formatter: "{value}",
+            },
+        },
+        yAxis: {
+            name: "Hue",
+            axisLabel: {
+                formatter: "{value}",
+            },
+        },
+
+        series: [
+            {
+                data: colorIntensity_hue,
+                type: "scatter",
+            },
+        ],
+    };
+
+    return (
+        <div className='app'>
+            <div className='app__charts'>
+                <ReactEcharts className='app__chart' option={optionScatter} />
+                <ReactEcharts className='app__chart' option={optionBar} />
+            </div>
+        </div>
+    );
+}
 export default App;
